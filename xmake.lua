d78c9a6a2fae4aeea3073c87d437d031c948758f
Qudix <17361645+Qudix@users.noreply.github.com>
2026-01-20 14:56:00 -0600

    feat: update lib

--- xmake.lua
+++ xmake.lua
@@ -1,22 +1,15 @@
-set_xmakever("2.8.2")
-
 includes("lib/commonlibf4")
 
 set_project("f4se-no-console-spam")
 set_version("0.1.1")
 set_license("GPL-3.0")
-
 set_languages("c++23")
 set_warnings("allextra")
-
-set_policy("package.requires_lock", true)
 
 add_rules("mode.debug", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
 
 target("f4se-no-console-spam")
-    add_deps("commonlibf4")
-
     add_rules("commonlibf4.plugin", {
         name = "no-console-spam",
         author = "qudix",
